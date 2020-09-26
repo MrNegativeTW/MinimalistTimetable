5e81e0d48ac54088118f9de1303dcde9dc522eec
TrevorWu <wowpapa3232@gmail.com>
2020-09-26 17:59:51 +0800

    d

--- course/app/src/main/java/com/txwstudio/app/timetable/DBHandler.java
+++ course/app/src/main/java/com/txwstudio/app/timetable/DBHandler.java
@@ -6,6 +6,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.txwstudio.app.timetable.Model.Course;
+
 import java.util.ArrayList;
 
 public class DBHandler extends SQLiteOpenHelper {
