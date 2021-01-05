ef7bc0002f01ee9cfe6cb0e14eed7f1ed0de4753
TrevorWu <wowpapa3232@gmail.com>
2021-01-05 23:58:42 +0800

    Reanme

--- course/app/src/main/java/com/txwstudio/app/timetable/model/Course2.kt
+++ course/app/src/main/java/com/txwstudio/app/timetable/model/Course2.kt
@@ -3,7 +3,7 @@ package com.txwstudio.app.timetable.model
 data class Course2(
         var courseName: String?,
         var coursePlace: String?,
-        var courseStartTime: String?,
+        var courseBeginTime: String?,
         var courseEndTime: String?,
         var courseTeacher: String?,
         var Id: Int?,
