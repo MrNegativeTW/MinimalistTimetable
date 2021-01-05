037bd26af40a307af5ab33b197debced94e2b8e0
TrevorWu <wowpapa3232@gmail.com>
2021-01-05 23:13:14 +0800

    Basic feature, CourseEditor

--- course/app/src/main/java/com/txwstudio/app/timetable/ui/courseeditor/CourseEditorActivity.kt
+++ course/app/src/main/java/com/txwstudio/app/timetable/ui/courseeditor/CourseEditorActivity.kt
@@ -1,6 +1,7 @@
 package com.txwstudio.app.timetable.ui.courseeditor
 
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -10,6 +11,8 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import com.txwstudio.app.timetable.R
 import com.txwstudio.app.timetable.databinding.ActivityCourseEditorBinding
 
@@ -25,13 +28,19 @@ class CourseEditorActivity : AppCompatActivity() {
 
     private var isEditMode = false
 
+    @TimeFormat
+    private var clockFormat = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_course_editor)
 
-//        binding.viewModel = courseEditorViewModel
+        binding.viewModel = courseEditorViewModel
 
         setupToolBar()
+
+        clockFormat = TimeFormat.CLOCK_12H
+
         subscribeUi()
     }
 
@@ -43,7 +52,7 @@ class CourseEditorActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.menuSave -> {
-                Log.i("TESTTT", "45646415")
+                binding.viewModel?.saveFired()
                 return true
             }
             android.R.id.home -> {
@@ -76,8 +85,50 @@ class CourseEditorActivity : AppCompatActivity() {
         /* Get the current day and set it as default when adding the course. */
         val autoWeekday = intent.getIntExtra("autoWeekday", 0)
 
-        val items = listOf("Material", "Design", "Components", "Android")
+        // Select weekday dialog
+        val items = resources.getStringArray(R.array.weekdayList)
         val adapter = ArrayAdapter(this, R.layout.list_item, items)
         (binding.dropDownCourseEditorAct as? AutoCompleteTextView)?.setAdapter(adapter)
+        binding.dropDownCourseEditorAct.setOnItemClickListener { adapterView, view, position, rowId ->
+            Log.i("TESTTT", "$position")
+        }
+
+        // Select course begin time
+        binding.dropDownCourseEditorActCourseBeginTime.setOnClickListener {
+            showMaterialTimePicker()
+        }
+
+        // Select course end time
+        binding.dropDownCourseEditorActCourseEndTime.setOnClickListener {
+            showMaterialTimePicker()
+        }
+
+        // Show Error
+        courseEditorViewModel.courseNameError.observe(this) {
+            if (it) {
+                binding.tilCourseEditorActCourseNameEntry.isErrorEnabled = true
+                binding.tilCourseEditorActCourseNameEntry.error = getString(R.string.courseEditor_noEntry)
+            } else {
+                binding.tilCourseEditorActCourseNameEntry.isErrorEnabled = false
+            }
+        }
+
+        courseEditorViewModel.coursePlaceError.observe(this) {
+            if (it) {
+                binding.tilCourseEditorActCoursePlaceEntry.isErrorEnabled = true
+                binding.tilCourseEditorActCoursePlaceEntry.error = getString(R.string.courseEditor_noEntry)
+            } else {
+                binding.tilCourseEditorActCoursePlaceEntry.isErrorEnabled = false
+            }
+        }
+    }
+
+    private fun showMaterialTimePicker() {
+        val isSystem24Hour = DateFormat.is24HourFormat(this)
+        clockFormat = if (isSystem24Hour) TimeFormat.CLOCK_24H else TimeFormat.CLOCK_12H
+        val materialTimePicker = MaterialTimePicker.Builder()
+                .setTimeFormat(clockFormat)
+                .build()
+        materialTimePicker.show(supportFragmentManager, "selectCourseBeginTime")
     }
 }
--- course/app/src/main/java/com/txwstudio/app/timetable/ui/courseeditor/CourseEditorViewModel.kt
+++ course/app/src/main/java/com/txwstudio/app/timetable/ui/courseeditor/CourseEditorViewModel.kt
@@ -1,7 +1,48 @@
 package com.txwstudio.app.timetable.ui.courseeditor
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class CourseEditorViewModel: ViewModel() {
-    // TODO: Implement the ViewModel
+class CourseEditorViewModel : ViewModel() {
+
+    var courseName = MutableLiveData<String>()
+    var coursePlace = MutableLiveData<String>()
+    var courseStartTime = MutableLiveData<String>()
+    var courseEndTime = MutableLiveData<String>()
+    var courseWeekday = MutableLiveData<String>()
+
+    var courseNameError = MutableLiveData<Boolean>()
+    var coursePlaceError = MutableLiveData<Boolean>()
+    var courseStartTimeError = MutableLiveData<Boolean>()
+    var courseEndTimeError = MutableLiveData<Boolean>()
+    var courseWeekdayError = MutableLiveData<Boolean>()
+
+    /**
+     * Check required entries are not empty.
+     *
+     * @return true, if EMPTY
+     * @return false, if NOT EMPTY
+     * */
+    private fun isRequiredEntriesEmpty(): Boolean {
+        return courseName.value.isNullOrEmpty() ||
+                coursePlace.value.isNullOrBlank()
+    }
+
+    private fun markEmptyEntries() {
+        courseNameError.value = courseName.value.isNullOrEmpty()
+        coursePlaceError.value = coursePlace.value.isNullOrEmpty()
+    }
+
+    /**
+     * User clicked save, start process it.
+     * */
+    fun saveFired() {
+        if (isRequiredEntriesEmpty()) {
+            markEmptyEntries()
+            return
+        } else {
+            markEmptyEntries()
+        }
+
+    }
 }
