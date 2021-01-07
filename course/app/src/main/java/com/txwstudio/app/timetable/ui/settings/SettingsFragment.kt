575b8fa3af433dbbfee5a4958650f0adb17e4ce6
TrevorWu <wowpapa3232@gmail.com>
2021-01-08 00:19:44 +0800

    Dialog for SettingsActivity

--- course/app/src/main/java/com/txwstudio/app/timetable/ui/settings/SettingsFragment.kt
+++ course/app/src/main/java/com/txwstudio/app/timetable/ui/settings/SettingsFragment.kt
@@ -4,7 +4,11 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.txwstudio.app.timetable.R
+
+private const val PREFERENCE_MAP_CAL_HELPER = "pref_mapCalHelper"
+private const val PREFERENCE_CHANGELOG = "pref_changelog"
 
 class SettingsFragment : PreferenceFragmentCompat(),
         Preference.OnPreferenceClickListener, SharedPreferences.OnSharedPreferenceChangeListener {
@@ -24,19 +28,52 @@ class SettingsFragment : PreferenceFragmentCompat(),
     }
 
     override fun onPreferenceClick(preference: Preference?): Boolean {
-        TODO("Not yet implemented")
+        TODO("Not yet implemented, also not called when click, what the hell is this?")
     }
 
     override fun onPreferenceTreeClick(preference: Preference?): Boolean {
+        when (preference?.key) {
+            "pref_mapCalHelper" -> {
+                showDialog(PREFERENCE_MAP_CAL_HELPER)
+            }
+            "pref_changelog" -> {
+                showDialog(PREFERENCE_CHANGELOG)
+            }
+        }
         return super.onPreferenceTreeClick(preference)
     }
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
         when (key) {
-            "tableTitle_Pref" -> {}
+            "tableTitle_Pref" -> {
+
+            }
             "lightMode_Pref" -> {
 
             }
+        }
+    }
+
+    /**
+     * Show information dialog.
+     * */
+    private fun showDialog(which: String) {
+        val materialAlertDialog = MaterialAlertDialogBuilder(requireContext())
+        val title = when (which) {
+            PREFERENCE_MAP_CAL_HELPER -> R.string.settings_mapCalendarHelperTitle
+            PREFERENCE_CHANGELOG -> R.string.settings_changelogTitle
+            else -> R.string.all_ohIsError
+        }
+        val message = when (which) {
+            PREFERENCE_MAP_CAL_HELPER -> R.string.settings_mapCalendarHelperMessage
+            PREFERENCE_CHANGELOG -> R.string.settings_changelogMessage
+            else -> R.string.all_ohIsError
+        }
+        materialAlertDialog.apply {
+            setTitle(title)
+            setMessage(message)
+            setPositiveButton(R.string.all_confirm) { _, _ -> }
+            show()
         }
     }
 }
