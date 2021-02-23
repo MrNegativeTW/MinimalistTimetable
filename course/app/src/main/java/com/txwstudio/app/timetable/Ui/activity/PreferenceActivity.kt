f9f2b88c3eb381658b7a9bc7e2b23dc97b4cfaa8
TrevorWu <wowpapa3232@gmail.com>
2021-02-24 00:07:14 +0800

    #6 [Add] Transition between preferences

--- course/app/src/main/java/com/txwstudio/app/timetable/Ui/activity/PreferenceActivity.kt
+++ course/app/src/main/java/com/txwstudio/app/timetable/Ui/activity/PreferenceActivity.kt
@@ -3,10 +3,14 @@ package com.txwstudio.app.timetable.ui.activity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
 import com.txwstudio.app.timetable.R
 import kotlinx.android.synthetic.main.activity_settings.*
 
-class PreferenceActivity : AppCompatActivity() {
+class PreferenceActivity : AppCompatActivity(),
+        PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +27,28 @@ class PreferenceActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onPreferenceStartFragment(caller: PreferenceFragmentCompat?, pref: Preference): Boolean {
+        // Instantiate the new Fragment
+        val args = pref.extras
+        val fragment = supportFragmentManager.fragmentFactory.instantiate(
+                classLoader,
+                pref.fragment)
+        fragment.arguments = args
+        fragment.setTargetFragment(caller, 0)
+
+        // Replace the existing Fragment with the new Fragment
+        supportFragmentManager.commit {
+            setCustomAnimations(
+                    R.anim.fade_in,
+                    R.anim.fade_out,
+                    R.anim.fade_in,
+                    R.anim.fade_out
+            )
+            replace(R.id.fragment_container_view, fragment)
+            addToBackStack(null)
+        }
+        return true
     }
 }
