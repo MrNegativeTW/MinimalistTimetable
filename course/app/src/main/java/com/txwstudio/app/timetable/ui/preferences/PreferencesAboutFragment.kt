fffe7946f2160a4cf5bfc49045e51009dee921a1
TrevorWu <wowpapa3232@gmail.com>
2021-02-24 13:17:24 +0800

    Add changelog link

--- course/app/src/main/java/com/txwstudio/app/timetable/ui/preferences/PreferencesAboutFragment.kt
+++ course/app/src/main/java/com/txwstudio/app/timetable/ui/preferences/PreferencesAboutFragment.kt
@@ -3,7 +3,6 @@ package com.txwstudio.app.timetable.ui.preferences
 import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
-import android.widget.Toast
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
@@ -15,7 +14,7 @@ private const val PREFERENCE_ABOUT_OPEN_SOURCE = "prefAbout_openSource"
 private const val PREFERENCE_ABOUT_OFFICIAL_SITE = "prefAbout_officialSite"
 private const val PREFERENCE_ABOUT_GITHUB = "prefAbout_github"
 
-private const val CHANGELOG_LINK = "prefAbout_changelog"
+private const val CHANGELOG_LINK = "https://github.com/MrNegativeTW/MinimalistTimetable/blob/master/CHANGELOG.md"
 private const val OPEN_SOURCE_LINK = "prefAbout_openSource"
 private const val OFFICIAL_SITE_LINK = "prefAbout_officialSite"
 private const val GITHUB_LINK = "https://github.com/MrNegativeTW/MinimalistTimetable"
@@ -33,7 +32,7 @@ class PreferencesAboutFragment : PreferenceFragmentCompat(),
 
     override fun onPreferenceTreeClick(preference: Preference?): Boolean {
         val customTabsIntent = CustomTabsIntent.Builder().build()
-        when(preference?.key) {
+        when (preference?.key) {
             PREFERENCE_ABOUT_CHANGELOG -> customTabsIntent.launchUrl(requireContext(), Uri.parse(CHANGELOG_LINK))
             PREFERENCE_ABOUT_OPEN_SOURCE -> customTabsIntent.launchUrl(requireContext(), Uri.parse(OPEN_SOURCE_LINK))
             PREFERENCE_ABOUT_OFFICIAL_SITE -> customTabsIntent.launchUrl(requireContext(), Uri.parse(OFFICIAL_SITE_LINK))
@@ -46,8 +45,6 @@ class PreferencesAboutFragment : PreferenceFragmentCompat(),
     override fun onSharedPreferenceChanged(p0: SharedPreferences?, p1: String?) {
         TODO("Not yet implemented")
     }
-
-
 
 
 }
