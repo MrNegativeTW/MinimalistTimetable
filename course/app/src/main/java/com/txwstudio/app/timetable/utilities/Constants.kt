6e31a31635f0f712dd4e08df5c6655651033dceb
TrevorWu <wowpapa3232@gmail.com>
2021-06-29 13:21:19 +0800

    Create Constants.kt

---
+++ course/app/src/main/java/com/txwstudio/app/timetable/utilities/Constants.kt
@@ -0,0 +1,7 @@
+package com.txwstudio.app.timetable.utilities
+
+/**
+ * Constants used throughout the app.
+ */
+const val DATABASE_NAME = "MyCourse.db"
+const val PLANT_DATA_FILENAME = "plants.json"
