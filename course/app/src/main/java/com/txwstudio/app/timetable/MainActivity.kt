e7ec100a1069b5944e5f5810f630f25262229895
TrevorWu <wowpapa3232@gmail.com>
2021-07-08 16:05:52 +0800

    Add MainActivity for single activity

---
+++ course/app/src/main/java/com/txwstudio/app/timetable/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.txwstudio.app.timetable
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil.setContentView
+import com.txwstudio.app.timetable.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+}
