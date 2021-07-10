eb293da9f45b33ccef1ab349efcb18c29bd360ba
TrevorWu <wowpapa3232@gmail.com>
2021-07-11 00:21:05 +0800

    Change view pager adapter param type from FragmentActivity to Fragment

--- course/app/src/main/java/com/txwstudio/app/timetable/adapter/CourseViewerPagerAdapter.kt
+++ course/app/src/main/java/com/txwstudio/app/timetable/adapter/CourseViewerPagerAdapter.kt
@@ -13,7 +13,7 @@ const val WEEKDAY_5 = 4
 const val WEEKDAY_6 = 5
 const val WEEKDAY_7 = 6
 
-class CourseViewerPagerAdapter(fragment: FragmentActivity, enableWeekend: Boolean) :
+class CourseViewerPagerAdapter(fragment: Fragment, enableWeekend: Boolean) :
         FragmentStateAdapter(fragment) {
 
     private val enableWeekends = enableWeekend
