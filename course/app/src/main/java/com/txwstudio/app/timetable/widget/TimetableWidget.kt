2d90746473066721acea851758fc71bc5f6129fd
TrevorWu <wowpapa3232@gmail.com>
2021-07-17 15:42:37 +0800

    Add example widget

---
+++ course/app/src/main/java/com/txwstudio/app/timetable/widget/TimetableWidget.kt
@@ -0,0 +1,37 @@
+package com.txwstudio.app.timetable.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+import com.txwstudio.app.timetable.R
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class TimetableWidget : AppWidgetProvider() {
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
+internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+    val widgetText = context.getString(R.string.title_activity_settings)
+    // Construct the RemoteViews object
+    val views = RemoteViews(context.packageName, R.layout.timetable_widget)
+    views.setTextViewText(R.id.appwidget_text, widgetText)
+
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
