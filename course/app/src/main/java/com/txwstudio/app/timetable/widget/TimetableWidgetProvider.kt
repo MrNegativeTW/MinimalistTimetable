a6a44a6204ba74a216426a890a2e76d8ecc9dc2c
TrevorWu <wowpapa3232@gmail.com>
2021-07-31 21:34:10 +0800

    Headline6 to Body1 app widget

--- course/app/src/main/java/com/txwstudio/app/timetable/widget/TimetableWidgetProvider.kt
+++ course/app/src/main/java/com/txwstudio/app/timetable/widget/TimetableWidgetProvider.kt
@@ -9,7 +9,7 @@ import com.txwstudio.app.timetable.R
 /**
  * Implementation of App Widget functionality.
  */
-class TimetableWidget : AppWidgetProvider() {
+class TimetableWidgetProvider : AppWidgetProvider() {
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
