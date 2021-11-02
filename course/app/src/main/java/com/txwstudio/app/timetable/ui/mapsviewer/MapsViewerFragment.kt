d1ae8c1126fe2df6d46dfa1e7e1b077ed0949574
TrevorWu <wowpapa3232@gmail.com>
2021-11-02 21:02:18 +0800

    Issue #28 Fix file not found crash

--- course/app/src/main/java/com/txwstudio/app/timetable/ui/mapsviewer/MapsViewerFragment.kt
+++ course/app/src/main/java/com/txwstudio/app/timetable/ui/mapsviewer/MapsViewerFragment.kt
@@ -7,6 +7,7 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.documentfile.provider.DocumentFile
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.preference.PreferenceManager
@@ -30,20 +31,9 @@ class MapsViewerFragment : Fragment() {
     ): View {
         binding = FragmentMapsViewerBinding.inflate(inflater, container, false)
 
-        val prefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
-        val mapPath = prefs.getString(PREFERENCE_MAP_PATH, "")
-
-        val mapUri = Uri.parse(mapPath)
-
-        mapPath?.isNotEmpty().let {
-            if (it == true) {
-                binding.textViewMapsViewerFragMapsNotFound.visibility = View.INVISIBLE
-                // TODO(May cause crash if the image doesn't exist)
-                binding.touchImageView.setImageURI(mapUri)
-            }
-        }
-
         setupToolBar()
+
+        loadMapImage()
 
         return binding.root
     }
@@ -63,6 +53,31 @@ class MapsViewerFragment : Fragment() {
         (activity as AppCompatActivity).supportActionBar?.setDisplayShowTitleEnabled(false)
         (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
         setHasOptionsMenu(true)
+    }
+
+    private fun loadMapImage() {
+        // Get map image path from shared preferences.
+        val prefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        val mapImagePath = prefs.getString(PREFERENCE_MAP_PATH, "")
+
+        // Check if map path string has empty value or not.
+        mapImagePath?.isNotEmpty()?.let {
+            // If it's empty, we treat it as not set, do nothing.
+            if (it) {
+                // Path string not empty, check file exists or not.
+                val mapImageUri = Uri.parse(mapImagePath)
+                checkMapImageExistsThenSetImage(mapImageUri)
+            }
+        }
+    }
+
+    private fun checkMapImageExistsThenSetImage(mapImageUri: Uri) {
+        DocumentFile.fromSingleUri(requireContext(), mapImageUri)?.exists()?.let {
+            if (it) {
+                binding.textViewMapsViewerFragMapsNotFound.visibility = View.INVISIBLE
+                binding.touchImageView.setImageURI(mapImageUri)
+            }
+        }
     }
 
 }
