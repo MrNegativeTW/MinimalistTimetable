5ca065f55c4dc008b3e968bcb743e4e34fff4dbb
TrevorWu <wowpapa3232@gmail.com>
2021-11-06 15:32:31 +0800

    Miss selected line working with the last commit

--- course/app/src/main/java/com/txwstudio/app/timetable/HomeViewPagerFragment.kt
+++ course/app/src/main/java/com/txwstudio/app/timetable/HomeViewPagerFragment.kt
@@ -31,7 +31,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [HomeViewPagerFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeViewPagerFragment : Fragment() {
+class HomeViewPagerFragment : Fragment(), SharedPreferences.OnSharedPreferenceChangeListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
