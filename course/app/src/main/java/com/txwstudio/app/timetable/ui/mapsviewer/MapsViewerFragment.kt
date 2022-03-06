e21207690a1b4d44eb7ee52b0624e03a36c6031c
TrevorWu <wowpapa3232@gmail.com>
2022-03-06 20:19:26 +0800

    Open PDF from MapsViewer if entry point isn't from HomeViewPager

--- course/app/src/main/java/com/txwstudio/app/timetable/ui/mapsviewer/MapsViewerFragment.kt
+++ course/app/src/main/java/com/txwstudio/app/timetable/ui/mapsviewer/MapsViewerFragment.kt
@@ -1,5 +1,8 @@
 package com.txwstudio.app.timetable.ui.mapsviewer
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -11,8 +14,13 @@ import androidx.documentfile.provider.DocumentFile
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.preference.PreferenceManager
+import com.google.android.material.snackbar.Snackbar
+import com.txwstudio.app.timetable.R
 import com.txwstudio.app.timetable.databinding.FragmentMapsViewerBinding
+import com.txwstudio.app.timetable.ui.preferences.PREFERENCE_MAP_DATA_TYPE
 import com.txwstudio.app.timetable.ui.preferences.PREFERENCE_MAP_PATH
+import com.txwstudio.app.timetable.utilities.DATA_TYPE_IMAGE
+import com.txwstudio.app.timetable.utilities.DATA_TYPE_PDF
 
 /**
  * Implementation of TouchImageView.
@@ -23,7 +31,19 @@ class MapsViewerFragment : Fragment() {
         fun newInstance() = MapsViewerFragment()
     }
 
+    private lateinit var sharedPref: SharedPreferences
+
     private lateinit var binding: FragmentMapsViewerBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Check image file type, open it up if it's pdf.
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        if (sharedPref.getString(PREFERENCE_MAP_DATA_TYPE, DATA_TYPE_IMAGE) == DATA_TYPE_PDF) {
+            openPdfThenNavigateUp()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -55,10 +75,41 @@ class MapsViewerFragment : Fragment() {
         setHasOptionsMenu(true)
     }
 
+    /**
+     * Open PDF when the file type is PDF and entry point isn't from HomeViewPagerFragment.
+     */
+    private fun openPdfThenNavigateUp() {
+        val mapPdfFileUriInString = sharedPref.getString(PREFERENCE_MAP_PATH, "")
+
+        val uri = Uri.parse(mapPdfFileUriInString)
+        val target = Intent(Intent.ACTION_VIEW).apply {
+            setDataAndType(uri, DATA_TYPE_PDF)
+            flags = Intent.FLAG_ACTIVITY_NO_HISTORY
+            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        }
+
+        val intent =
+            Intent.createChooser(target, java.lang.String.valueOf(R.string.pdfOpenWithMsg))
+
+        try {
+            startActivity(intent)
+            findNavController().navigateUp()
+        } catch (e: ActivityNotFoundException) {
+            // Suitable app not found.
+            Snackbar.make(requireView(), R.string.pdfNoAppMsg, Snackbar.LENGTH_SHORT).show()
+        } catch (e: SecurityException) {
+            // File not found.
+            Snackbar.make(requireView(), R.string.pdfFileNotFound, Snackbar.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            // Unknown exception.
+            Snackbar.make(requireView(), R.string.fileReadErrorMsg, Snackbar.LENGTH_SHORT)
+                .show()
+        }
+    }
+
     private fun loadMapImage() {
         // Get map image path from shared preferences.
-        val prefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
-        val mapImagePath = prefs.getString(PREFERENCE_MAP_PATH, "")
+        val mapImagePath = sharedPref.getString(PREFERENCE_MAP_PATH, "")
 
         // Check if map path string has empty value or not.
         mapImagePath?.isNotEmpty()?.let {
@@ -71,6 +122,11 @@ class MapsViewerFragment : Fragment() {
         }
     }
 
+    /**
+     * Check image file exist before setImage to prevent crashes.
+     *
+     * @param mapImageUri Parsed file uri.
+     * */
     private fun checkMapImageExistsThenSetImage(mapImageUri: Uri) {
         DocumentFile.fromSingleUri(requireContext(), mapImageUri)?.exists()?.let {
             if (it) {
